0be6d9be35ca48fb635fbbeb955d53d1178ad953
lookrecius <bragin.boris@ya.ru>
2020-03-30 05:38:35 +0300

    Redeploy sapper in legacy

---
+++ __sapper__/build/client/legacy/client.d16b46c4.js
@@ -0,0 +1,1 @@
+import"./client.a5da721f.js";
---
+++ __sapper__/dev/client/client.a912674f.js
@@ -0,0 +1,2 @@
+import './client.7fc19869.js';
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY2xpZW50LmE5MTI2NzRmLmpzIiwic291cmNlcyI6W10sInNvdXJjZXNDb250ZW50IjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiJ9
