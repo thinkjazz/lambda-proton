13d79034e25f0db3d5945d8f3c7ce0ff6bb45584
lookrecius <bragin.boris@ya.ru>
2020-04-04 04:59:03 +0300

    Create buttons.svelte page

---
+++ src/routes/ui/buttons.svelte
@@ -0,0 +1,92 @@
+<script>
+    import  Breadcrumb  from "sveltestrap/src/Breadcrumb.svelte";
+    import  BreadcrumbItem  from "sveltestrap/src/BreadcrumbItem.svelte";
+    import  Row  from "sveltestrap/src/Row.svelte";
+    import  { Button, ButtonGroup, ButtonToolbar }  from "sveltestrap";
+
+    const colors = [
+        "primary",
+        "secondary",
+        "success",
+        "danger",
+        "warning",
+        "info",
+        "light",
+        "dark"
+    ];
+
+
+    let title = "Кнопки";
+</script>
+
+<svelte:head>
+    <title>{title}</title>
+</svelte:head>
+<h1 class="mt-4">Кнопки</h1>
+<Breadcrumb class="mb-4">
+    <BreadcrumbItem active>Кнопки</BreadcrumbItem>
+</Breadcrumb>
+<Row>
+    <div class="col-xl-6">
+        <h1 class="mt-4">Цвета кнопок</h1>
+        {#each colors as color}
+            <div>
+                <Button {color}>
+                    {color}
+                </Button>
+            </div>
+        {/each}
+    </div>
+</Row>
+<Row>
+    <div class="col-xl-6">
+        <h1 class="mt-4">Кнопки с контурами</h1>
+        {#each colors as color}
+            <div>
+                <Button outline {color}>
+                    {color}
+                </Button>
+            </div>
+        {/each}
+    </div>
+</Row>
+<Row>
+    <div class="col-xl-6">
+        <h1 class="mt-4">Неактивные</h1>
+        {#each colors as color}
+            <div>
+                <Button disabled {color}>
+                    {color}
+                </Button>
+            </div>
+        {/each}
+    </div>
+</Row>
+<Row>
+    <div class="col-xl-6">
+        <h1 class="mt-4">Размеры</h1>
+        <Button size="lg">Large</Button>
+        <Button size="sm">Small</Button>
+        <Button block>Block</Button>
+    </div>
+</Row>
+<Row>
+    <div class="col-xl-6">
+        <h1 class="mt-4">Группы кнопок</h1>
+        <ButtonGroup>
+            <Button active>Alpha</Button>
+            <Button>Bravo</Button>
+            <Button>Charlie</Button>
+        </ButtonGroup>
+    </div>
+</Row>
+<Row>
+    <div class="col-xl-6">
+        <h1 class="mt-4">Тулбар</h1>
+        <ButtonToolbar>
+            <Button>Alpha</Button>
+            <Button>Bravo</Button>
+            <Button>Charlie</Button>
+        </ButtonToolbar>
+    </div>
+</Row>
