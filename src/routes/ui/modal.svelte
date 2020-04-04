69c17d0faf5588474da99ae994ddb8521c0a3308
lookrecius <bragin.boris@ya.ru>
2020-04-05 01:01:33 +0300

    Add modal page

---
+++ src/routes/ui/modal.svelte
@@ -0,0 +1,47 @@
+<script>
+    import  Breadcrumb  from "sveltestrap/src/Breadcrumb.svelte";
+    import  BreadcrumbItem  from "sveltestrap/src/BreadcrumbItem.svelte";
+    import  Row  from "sveltestrap/src/Row.svelte";
+    import  Button from "sveltestrap/src/Button.svelte";
+    import  Modal from "sveltestrap/src/Modal.svelte";
+    import  ModalBody from "sveltestrap/src/ModalBody.svelte";
+    import  ModalFooter from "sveltestrap/src/ModalFooter.svelte";
+    import  ModalHeader from "sveltestrap/src/ModalHeader.svelte";
+    let open = false;
+    const toggle = () => (open = !open);
+
+    let title = "Модальные окна | UI | Пользовательский Интерфейс";
+</script>
+
+<svelte:head>
+    <title>{title}</title>
+</svelte:head>
+<h1 class="mt-4">Модальные окна</h1>
+<Breadcrumb class="mb-4">
+    <BreadcrumbItem active>Группы кнопок</BreadcrumbItem>
+</Breadcrumb>
+<Row>
+    <div class="col-xl-6">
+        <h2 class="mt-4">Модальное окно</h2>
+        <div>
+            <Button color="danger" on:click={toggle}>
+                Открыть модальное окно
+            </Button>
+            <Modal isOpen={open} {toggle}>
+                <ModalHeader {toggle}>Заголовок</ModalHeader>
+                <ModalBody>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                </ModalBody>
+                <ModalFooter>
+                    <Button color="primary" on:click={toggle}>
+                        Ок
+                    </Button>
+                    <Button color="secondary" on:click={toggle}>
+                        Отмена
+                    </Button>
+                </ModalFooter>
+            </Modal>
+        </div>
+    </div>
+</Row>
---
+++ src/routes/ui/spinners.svelte
@@ -0,0 +1,61 @@
+<script>
+    import  Breadcrumb  from "sveltestrap/src/Breadcrumb.svelte";
+    import  BreadcrumbItem  from "sveltestrap/src/BreadcrumbItem.svelte";
+    import  Row  from "sveltestrap/src/Row.svelte";
+      import { Spinner } from "sveltestrap";
+    const colors = [
+        "primary",
+        "secondary",
+        "success",
+        "danger",
+        "warning",
+        "info",
+        "light",
+        "dark"
+    ];
+
+
+    let title = "Загрузчики | UI | Пользовательский Интерфейс";
+</script>
+
+<svelte:head>
+    <title>{title}</title>
+</svelte:head>
+<h1 class="mt-4">Загрузчики</h1>
+<Breadcrumb class="mb-4">
+    <BreadcrumbItem active>Загрузчики</BreadcrumbItem>
+</Breadcrumb>
+<Row>
+    <div class="col-xl-6">
+        <h2 class="mt-4">Цвета</h2>
+        {#each colors as color}
+            <Spinner {color} />
+        {/each}
+    </div>
+</Row>
+<Row>
+    <div class="col-xl-6">
+        <h2 class="mt-4">Типы</h2>
+
+        <div>
+            {#each colors as color}
+                <Spinner {color} type="border" />
+            {/each}
+        </div>
+
+        <div>
+            {#each colors as color}
+                <Spinner {color} type="grow" />
+            {/each}
+        </div>
+    </div>
+</Row>
+<Row>
+    <div class="col-xl-6">
+        <h2 class="mt-4">Размеры</h2>
+
+        <Spinner size="sm" />
+        <Spinner size="sm" type="grow" />
+
+    </div>
+</Row>
