0f24d0832a5b7e3b1f44d6b6988e0cfd7dab7fbd
lookrecius <bragin.boris@ya.ru>
2020-04-04 16:33:59 +0300

    Added button group page and added ui elements src/routes/ui/buttonGroup.svelte

--- src/routes/ui/buttonGroup.svelte
+++ src/routes/ui/buttonGroup.svelte
@@ -1,59 +1,38 @@
-<!--<script>-->
-<!--    import Card from "sveltestrap/src/Card.svelte";-->
-<!--    import CardBody from "sveltestrap/src/CardBody.svelte";-->
-<!--    import CardHeader from "sveltestrap/src/CardHeader.svelte";-->
-<!--    import CardFooter from "sveltestrap/src/CardFooter.svelte";-->
-<!--    import Form from "sveltestrap/src/Form.svelte";-->
-<!--    import FormGroup from "sveltestrap/src/FormGroup.svelte";-->
-<!--    import Label from "sveltestrap/src/Label.svelte";-->
-<!--    import Input from "sveltestrap/src/Input.svelte";-->
-<!--    import CustomInput from "sveltestrap/src/CustomInput.svelte";-->
-<!--    import Button from "sveltestrap/src/Button.svelte";-->
+<script>
+    import  Breadcrumb  from "sveltestrap/src/Breadcrumb.svelte";
+    import  BreadcrumbItem  from "sveltestrap/src/BreadcrumbItem.svelte";
+    import  Row  from "sveltestrap/src/Row.svelte";
+    import  Button from "sveltestrap/src/Button.svelte";
+    import  ButtonGroup from "sveltestrap/src/ButtonGroup.svelte";
+    const colors = [
+        "primary",
+        "secondary",
+        "success",
+        "danger",
+        "warning",
+        "info",
+        "light",
+        "dark"
+    ];
 
-<!--</script>-->
 
-<!--<div class="col-lg-5">-->
-<!--    <Card class="shadow-lg border-0 rounded-lg mt-5">-->
-<!--        <CardHeader>-->
-<!--            <h3 class="text-center font-weight-light my-4">Логин</h3>-->
-<!--        </CardHeader>-->
-<!--        <CardBody>-->
-<!--            <Form>-->
-<!--                <FormGroup>-->
-<!--                    <Label for="exampleEmail" class="small mb-1">Email</Label>-->
-<!--                    <Input-->
-<!--                            class="py-4"-->
-<!--                            type="email"-->
-<!--                            name="email"-->
-<!--                            id="exampleEmail"-->
-<!--                            placeholder="Введите адрес электронной почты" />-->
-<!--                </FormGroup>-->
-<!--                <FormGroup>-->
-<!--                    <Label for="examplePassword" class="small mb-1">Пароль</Label>-->
-<!--                    <Input-->
-<!--                            class="py-4"-->
-<!--                            type="password"-->
-<!--                            name="password"-->
-<!--                            id="examplePassword"-->
-<!--                            placeholder="Введите пароль" />-->
-<!--                </FormGroup>-->
-<!--                <FormGroup>-->
-<!--                    <CustomInput-->
-<!--                            type="checkbox"-->
-<!--                            id="exampleCustomCheckbox"-->
-<!--                            label="Запомнить пароль" />-->
-<!--                </FormGroup>-->
-<!--                <FormGroup-->
-<!--                        class="d-flex align-items-center justify-content-between mt-4 mb-0">-->
-<!--                    <a class="small" href="pages/authentication/forget_password">-->
-<!--                        Забыли пароль?-->
-<!--                    </a>-->
-<!--                    <Button color="primary" href=".">Вход</Button>-->
-<!--                </FormGroup>-->
-<!--            </Form>-->
-<!--        </CardBody>-->
-<!--        <CardFooter class="text-center small">-->
-<!--            <a href="pages/authentication/register">Нужен аккаунт? Зарегистрируйтесь!</a>-->
-<!--        </CardFooter>-->
-<!--    </Card>-->
-<!--</div>-->
+    let title = "Кнопки";
+</script>
+
+<svelte:head>
+    <title>{title}</title>
+</svelte:head>
+<h1 class="mt-4">Группы кнопок</h1>
+<Breadcrumb class="mb-4">
+    <BreadcrumbItem active>Группы кнопок</BreadcrumbItem>
+</Breadcrumb>
+<Row>
+    <div class="col-xl-6">
+        <h1 class="mt-4">Группы кнопок</h1>
+        <ButtonGroup>
+            <Button active>Alpha</Button>
+            <Button>Bravo</Button>
+            <Button>Charlie</Button>
+        </ButtonGroup>
+    </div>
+</Row>
