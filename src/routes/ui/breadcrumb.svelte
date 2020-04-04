39c1cf5cce13ee0f015b0f9dced9c444c5035968
lookrecius <bragin.boris@ya.ru>
2020-04-04 16:32:20 +0300

    Added breadcrumbs route and page src/routes/ui/breadcrumb.svelte

---
+++ src/routes/ui/breadcrumb.svelte
@@ -0,0 +1,42 @@
+<script>
+    import  Breadcrumb  from "sveltestrap/src/Breadcrumb.svelte";
+    import  BreadcrumbItem  from "sveltestrap/src/BreadcrumbItem.svelte";
+    import  Row  from "sveltestrap/src/Row.svelte";
+
+
+
+    let title = "Кнопки";
+</script>
+
+<svelte:head>
+    <title>{title}</title>
+</svelte:head>
+<h1 class="mt-4">Хлебные крошки</h1>
+<Breadcrumb class="mb-4">
+    <BreadcrumbItem active>Хлебные крошки</BreadcrumbItem>
+</Breadcrumb>
+<Row>
+    <div class="col-xl-6">
+        <h1 class="mt-4">Хлебные крошки</h1>
+        <Breadcrumb>
+            <BreadcrumbItem active>Домой</BreadcrumbItem>
+        </Breadcrumb>
+
+        <Breadcrumb>
+            <BreadcrumbItem>
+                <a href="#home">Домой</a>
+            </BreadcrumbItem>
+            <BreadcrumbItem active>Библиотека</BreadcrumbItem>
+        </Breadcrumb>
+
+        <Breadcrumb>
+            <BreadcrumbItem>
+                <a href="#home">Домой</a>
+            </BreadcrumbItem>
+            <BreadcrumbItem>
+                <a href="#library">Библиотека</a>
+            </BreadcrumbItem>
+            <BreadcrumbItem active>Данные</BreadcrumbItem>
+        </Breadcrumb>
+    </div>
+</Row>
