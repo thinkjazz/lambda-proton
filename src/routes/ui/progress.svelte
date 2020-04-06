15a45da23240d2a5eb824921162b58d245c6f5dc
lookrecius <bragin.boris@ya.ru>
2020-04-07 00:34:12 +0300

    AdAdded animated progress bar component to src/routes/ui/progress.svelte

--- src/routes/ui/progress.svelte
+++ src/routes/ui/progress.svelte
@@ -95,3 +95,20 @@
         </Progress>
     </div>
 </Row>
+<Row>
+    <div class="col-xl-6">
+        <h2 class="mt-4">Анимированные</h2>
+        <h3 class="mt-4">Пример</h3>
+        <Progress animated value={2 * 5} />
+        <Progress animated color="success" value="25" />
+        <Progress animated color="info" value={50} />
+        <Progress animated color="warning" value={75} />
+        <Progress animated color="danger" value="100" />
+        <Progress multi>
+            <Progress animated bar value="10" />
+            <Progress animated bar color="success" value="30" />
+            <Progress animated bar color="warning" value="20" />
+            <Progress animated bar color="danger" value="20" />
+        </Progress>
+    </div>
+</Row>
