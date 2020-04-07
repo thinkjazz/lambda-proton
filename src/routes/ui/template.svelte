ddb72181337f76e765b831d4f36be54f949260ea
lookrecius <bragin.boris@ya.ru>
2020-04-07 16:26:25 +0300

    Added paragraph style to template.svelte

--- src/routes/ui/template.svelte
+++ src/routes/ui/template.svelte
@@ -1,5 +1,5 @@
 <script>
-    let title = "Пагинация | UI | Пользовательский Интерфейс";
+    let title = "ШАБЛОН | UI | Пользовательский Интерфейс";
     import  Breadcrumb  from "sveltestrap/src/Breadcrumb.svelte";
     import  BreadcrumbItem  from "sveltestrap/src/BreadcrumbItem.svelte";
     import  Row  from "sveltestrap/src/Row.svelte";
@@ -23,13 +23,35 @@
 <svelte:head>
     <title>{title}</title>
 </svelte:head>
-<h1 class="mt-4">Группы кнопок</h1>
+<h1 class="mt-4">ШАБЛОН</h1>
 <Breadcrumb class="mb-4">
-    <BreadcrumbItem active>Группы кнопок</BreadcrumbItem>
+    <BreadcrumbItem active>ШАБЛОН</BreadcrumbItem>
 </Breadcrumb>
 <Row>
     <div class="col-xl-6">
-        <h2 class="mt-4">Группы кнопок</h2>
-
+        <h2 class="mt-4">ШАБЛОН</h2>
+        <h3 class="mt-4">Пример</h3>
     </div>
+</Row>
+
+<Row>
+    <div class="col-xl-6">
+        <h2 class="mt-4">ШАБЛОН</h2>
+        <h3 class="mt-4">Пример</h3>
+<p class="lead">
+    Переключайте видимость контента в вашем проекте
+    с помощью нескольких классов и наших плагинов JavaScript.
+</p>
+<p><small>Как это работает
+    Плагин JavaScript используется для отображения и скрытия
+    содержимого.
+    Кнопки или якоря используются в качестве триггеров,
+    которые привязываются к определенным элементам, которые
+    вы переключите.
+    Свертывание элемента анимирует высоту от текущего значения до 0.
+    Учитывая, как CSS обрабатывает анимацию, вы не можете использовать подкладку на .свертывающемся элементе. Вместо этого используйте класс в качестве независимого оберточного элемента.
+    Эффект анимации этого элемента зависит от медиа-запроса
+    "Предпочтение - уменьшение движения".</small></p>
+</div>
+
 </Row>
