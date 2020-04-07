e7261f035f27d45d51f89036c4c8bef17a7c7844
lookrecius <bragin.boris@ya.ru>
2020-04-07 18:30:14 +0300

    Fixed: layout in src/routes/ui/input.svelte

--- src/routes/ui/input.svelte
+++ src/routes/ui/input.svelte
@@ -25,135 +25,139 @@
     <BreadcrumbItem active>Поле ввода Input</BreadcrumbItem>
 </Breadcrumb>
 <Row>
-    <div class="col-xl-6">
-        <h2 class="mt-4">Поле ввода Input</h2>
-        <Form>
-            <FormGroup>
-                <Label for="exampleEmail">Простой текст (статический)</Label>
-                <Input plaintext value="Некоторый обычный текст/статическое значение" />
-            </FormGroup>
-            <FormGroup>
-                <Label for="exampleEmail">Email</Label>
-                <Input
-                        type="email"
-                        name="email"
-                        id="exampleEmail"
-                        placeholder="Введите почту" />
-            </FormGroup>
-            <FormGroup>
-                <Label for="examplePassword">Пароль</Label>
-                <Input
-                        type="password"
-                        name="password"
-                        id="examplePassword"
-                        placeholder="Введите пароль" />
-            </FormGroup>
-            <FormGroup>
-                <Label for="exampleUrl">Url</Label>
-                <Input
-                        type="url"
-                        name="url"
-                        id="exampleUrl"
-                        placeholder="введите url" />
-            </FormGroup>
-            <FormGroup>
-                <Label for="exampleNumber">Номер</Label>
-                <Input
-                        type="number"
-                        name="number"
-                        id="exampleNumber"
-                        placeholder="заполните номер" />
-            </FormGroup>
-            <FormGroup>
-                <Label for="exampleDatetime">Сроки</Label>
-                <Input
-                        type="datetime"
-                        name="datetime"
-                        id="exampleDatetime"
-                        placeholder="Введите сроки" />
-            </FormGroup>
-            <FormGroup>
-                <Label for="exampleDate">Дата</Label>
-                <Input
-                        type="date"
-                        name="date"
-                        id="exampleDate"
-                        placeholder="Введите дату" />
-            </FormGroup>
-            <FormGroup>
-                <Label for="exampleTime">Время</Label>
-                <Input
-                        type="time"
-                        name="time"
-                        id="exampleTime"
-                        placeholder="Введите время" />
-            </FormGroup>
-            <FormGroup>
-                <Label for="exampleColor">Цвет</Label>
-                <Input
-                        type="color"
-                        name="color"
-                        id="exampleColor"
-                        placeholder="Цветовой плейсхолдер" />
-            </FormGroup>
-            <FormGroup>
-                <Label for="exampleSearch">Поиск</Label>
-                <Input
-                        type="search"
-                        name="search"
-                        id="exampleSearch"
-                        placeholder="Введите запрос для поиска" />
-            </FormGroup>
-            <FormGroup>
-                <Label for="exampleSelect">Выбор(Селектор)</Label>
-                <Input type="select" name="select" id="exampleSelect">
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
-                    <option>5</option>
-                </Input>
-            </FormGroup>
-            <FormGroup>
-                <Label for="exampleSelectMulti">Выбор нескольких параметров</Label>
-                <Input type="select" name="selectMulti" id="exampleSelectMulti" multiple>
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
-                    <option>5</option>
-                </Input>
-            </FormGroup>
-            <FormGroup>
-                <Label for="exampleText">Текстовая область</Label>
-                <Input type="textarea" name="text" id="exampleText" />
-            </FormGroup>
-            <FormGroup>
-                <Label for="exampleFile">Файл</Label>
-                <Input type="file" name="file" id="exampleFile" />
-                <FormText color="muted">
-                Это какой-то блок-текст справки на уровне плейсхолдеров для вышеприведенного ввода.
-                Это немного легче и легко обертывается в новую линию.
-                </FormText>
-            </FormGroup>
-            <FormGroup check>
-                <Label check>
-                    <Input type="radio" />
-                    Вариант первый - это и то, и то, и другое - обязательно включите, почему это здорово.
-                </Label>
-            </FormGroup>
-            <FormGroup check>
-                <Label check>
-                    <Input type="checkbox" />
-                    Проверьте меня
-                </Label>
-            </FormGroup>
-        </Form>
+    <div class="col-xl-12">
+        <h2 class="mt-4">Поле ввода</h2>
+        <h4 class="mt-4">Пример</h4>
+    <Form>
+        <FormGroup>
+            <Label for="exampleEmail">Простой текст (статический)</Label>
+            <Input plaintext value="Некоторый обычный текст/статическое значение" />
+        </FormGroup>
+        <FormGroup>
+            <Label for="exampleEmail">Email</Label>
+            <Input
+                    type="email"
+                    name="email"
+                    id="exampleEmail"
+                    placeholder="Введите почту" />
+        </FormGroup>
+        <FormGroup>
+            <Label for="examplePassword">Пароль</Label>
+            <Input
+                    type="password"
+                    name="password"
+                    id="examplePassword"
+                    placeholder="Введите пароль" />
+        </FormGroup>
+        <FormGroup>
+            <Label for="exampleUrl">Url</Label>
+            <Input
+                    type="url"
+                    name="url"
+                    id="exampleUrl"
+                    placeholder="введите url" />
+        </FormGroup>
+        <FormGroup>
+            <Label for="exampleNumber">Номер</Label>
+            <Input
+                    type="number"
+                    name="number"
+                    id="exampleNumber"
+                    placeholder="заполните номер" />
+        </FormGroup>
+        <FormGroup>
+            <Label for="exampleDatetime">Сроки</Label>
+            <Input
+                    type="datetime"
+                    name="datetime"
+                    id="exampleDatetime"
+                    placeholder="Введите сроки" />
+        </FormGroup>
+        <FormGroup>
+            <Label for="exampleDate">Дата</Label>
+            <Input
+                    type="date"
+                    name="date"
+                    id="exampleDate"
+                    placeholder="Введите дату" />
+        </FormGroup>
+        <FormGroup>
+            <Label for="exampleTime">Время</Label>
+            <Input
+                    type="time"
+                    name="time"
+                    id="exampleTime"
+                    placeholder="Введите время" />
+        </FormGroup>
+        <FormGroup>
+            <Label for="exampleColor">Цвет</Label>
+            <Input
+                    type="color"
+                    name="color"
+                    id="exampleColor"
+                    placeholder="Цветовой плейсхолдер" />
+        </FormGroup>
+        <FormGroup>
+            <Label for="exampleSearch">Поиск</Label>
+            <Input
+                    type="search"
+                    name="search"
+                    id="exampleSearch"
+                    placeholder="Введите запрос для поиска" />
+        </FormGroup>
+        <FormGroup>
+            <Label for="exampleSelect">Выбор(Селектор)</Label>
+            <Input type="select" name="select" id="exampleSelect">
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+            </Input>
+        </FormGroup>
+        <FormGroup>
+            <Label for="exampleSelectMulti">Выбор нескольких параметров</Label>
+            <Input type="select" name="selectMulti" id="exampleSelectMulti" multiple>
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+            </Input>
+        </FormGroup>
+        <FormGroup>
+            <Label for="exampleText">Текстовая область</Label>
+            <Input type="textarea" name="text" id="exampleText" />
+        </FormGroup>
+        <FormGroup>
+            <Label for="exampleFile">Файл</Label>
+            <Input type="file" name="file" id="exampleFile" />
+            <FormText color="muted">
+            Это какой-то блок-текст справки на уровне плейсхолдеров для вышеприведенного ввода.
+            Это немного легче и легко обертывается в новую линию.
+            </FormText>
+        </FormGroup>
+        <FormGroup check>
+            <Label check>
+                <Input type="radio" />
+                Вариант первый - это и то, и то, и другое - обязательно включите, почему это здорово.
+            </Label>
+        </FormGroup>
+        <FormGroup check>
+            <Label check>
+                <Input type="checkbox" />
+                Проверьте меня
+            </Label>
+        </FormGroup>
+    </Form>
     </div>
+
+
 </Row>
 <Row>
-    <div class="col-xl-6">
+    <div class="col-xl-12">
         <h2 class="mt-4">Связывание: (Binding)</h2>
+        <h4 class="mt-4">Пример:</h4>
         <p>Рекомендуемый способ привязки значений к входам - ​​через <code> bind: value = yourBind в фигурных скобках </code></p>
         <FormGroup>
             <Label>Начните печать тут</Label>
@@ -165,8 +169,9 @@
     </div>
 </Row>
 <Row>
-    <div class="col-xl-6">
-        <h2 class="mt-4">Привязка событий (Event Binding) </h2>
+    <div class="col-xl-12">
+        <h2 class="mt-4">Привязка событий (Event Binding)</h2>
+        <h4 class="mt-4">Пример:</h4>
         <p>Вы также можете связать непосредственно с обработчиками событий: <code>on:blur on:focus on:keydown on:keyup on:change on:input</code>
             но это не рекомендуется, если вы просто привязаны к простому значению - Svelte не реагирует. :-)
             Если вам нужно больше экзотических событий, пожалуйста, следуйте номер <a href="https://github.com/bestguy/sveltestrap/issues/36">36.</a></p>
