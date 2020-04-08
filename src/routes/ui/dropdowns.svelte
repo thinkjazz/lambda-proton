8cb74bf893f03b5b7392c9ea22ea9dcc94070ff8
lookrecius <bragin.boris@ya.ru>
2020-04-09 01:33:36 +0300

    Added: Uncontrolled dropwond and navbar dropdown

--- src/routes/ui/dropdowns.svelte
+++ src/routes/ui/dropdowns.svelte
@@ -8,6 +8,12 @@
     import  DropdownItem from "sveltestrap/src/DropdownItem.svelte";
     import  DropdownMenu from "sveltestrap/src/DropdownMenu.svelte";
     import  DropdownToggle from "sveltestrap/src/DropdownToggle.svelte";
+    import  UncontrolledDropdown from "sveltestrap/src/UncontrolledDropdown.svelte";
+
+    import  Nav from "sveltestrap/src/Nav.svelte";
+    import  NavItem from "sveltestrap/src/NavItem.svelte";
+    import  NavLink from "sveltestrap/src/NavLink.svelte";
+    import  Navbar  from "sveltestrap/src/Navbar.svelte";
 
     const colors = [
         "primary",
@@ -19,9 +25,13 @@
         "light",
         "dark"
     ];
+    
     let isOpen = false;
     let leftIsOpen = false;
     let rightIsOpen = false;
+
+    let lgIsOpen = false;
+    let smIsOpen = false;
 
 </script>
 
@@ -126,4 +136,83 @@
             </DropdownMenu>
         </Dropdown>
     </div>
+</Row>
+<Row>
+    <div class="col-xl-6">
+          <h3 class="mt-4">Размер</h3>
+          <h4 class="mt-4">Пример</h4>
+          <div class="mb-3">
+  <Dropdown
+    group
+    isOpen={lgIsOpen}
+    size="lg"
+    toggle={() => (lgIsOpen = !lgIsOpen)}
+  >
+    <DropdownToggle caret>Large Dropdown</DropdownToggle>
+    <DropdownMenu>
+      <DropdownItem>Shave the modern way</DropdownItem>
+    </DropdownMenu>
+  </Dropdown>
+</div>
+
+<div class="mb-3">
+  <Dropdown {isOpen} toggle={() => (isOpen = !isOpen)}>
+    <DropdownToggle caret>Dropdown</DropdownToggle>
+    <DropdownMenu>
+      <DropdownItem>No Brush, No Lather</DropdownItem>
+    </DropdownMenu>
+  </Dropdown>
+</div>
+
+<div class="mb-3">
+  <Dropdown
+    group
+    isOpen={smIsOpen}
+    size="sm"
+    toggle={() => (smIsOpen = !smIsOpen)}
+  >
+    <DropdownToggle caret>Small Dropdown</DropdownToggle>
+    <DropdownMenu>
+      <DropdownItem>Burma-Shave</DropdownItem>
+    </DropdownMenu>
+  </Dropdown>
+</div>
+
+    </div>
+     <div class="col-xl-6">
+          <h3 class="mt-4">Независимый</h3>
+          <h4 class="mt-4">Пример</h4>
+<UncontrolledDropdown>
+  <DropdownToggle caret>Dropdown</DropdownToggle>
+      <DropdownMenu>
+        <DropdownItem header>Header</DropdownItem>
+        <DropdownItem disabled>Action</DropdownItem>
+        <DropdownItem>Another Action</DropdownItem>
+        <DropdownItem divider />
+        <DropdownItem>Another Action</DropdownItem>
+      </DropdownMenu>
+</UncontrolledDropdown>
+    </div>
+</Row>
+<Row>
+     <div class="col-xl-12">
+          <h3 class="mt-4">SetActiveFromChild</h3>
+          <h4 class="mt-4">Пример</h4>
+<Navbar color="light" light expand="md">
+  <Nav class="ml-auto" navbar>
+    <NavItem>
+      <NavLink href="/components/">Inactive Link</NavLink>
+    </NavItem>
+    <UncontrolledDropdown setActiveFromChild>
+      <DropdownToggle nav class="nav-link" caret>Dropdown</DropdownToggle>
+      <DropdownMenu>
+        <DropdownItem tag="a" href="/blah" active>Link</DropdownItem>
+      </DropdownMenu>
+    </UncontrolledDropdown>
+  </Nav>
+</Navbar>
+    </div>
+</Row>
+<Row>
+  <div class="col-xl-12"><br></div>
 </Row>
